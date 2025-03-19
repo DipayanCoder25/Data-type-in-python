67dea868067d3e0b5b12eaba54bc1331e6b5f595
DipayanCoder25 <dipayanprogramer3@gmail.com>
2025-03-19 14:45:12 +0600

    Add files via upload

---
+++ prctice2.py
@@ -0,0 +1,8 @@
+name = input("Enter  name: ")
+achievement = input("Enter the achievement: ")
+
+message = f"Congrats, {name}! 🎉\n You have successfully achieved {achievement}. "
+
+print( "\n",message)
+do=achievement[0:-1]
+print(f"Very Good {name} \n You earned {do}")
