bc6c35f32a0c2c3316c7d72710ff7cb47a8c3e0e
DipayanCoder25 <dipayanprogramer3@gmail.com>
2025-03-19 19:54:13 +0600

    Add files via upload

--- prctice2.py
+++ prctice2.py
@@ -1,8 +1,22 @@
-name = input("Enter  name: ")
-achievement = input("Enter the achievement: ")
+a1=int(input("enter the number"))
+a2=int(input("Enter the 2nd number"))
+a3=int(input("Enter the3rd number"))
+sum=a1+a2+a3
+percentage=(sum/300)*100
+print(percentage)
 
-message = f"Congrats, {name}! 🎉\n You have successfully achieved {achievement}. "
+'''
+notes_of_100=amount//100
+remaining_of_100=amount%100
+notes_of_50=remaining_of_100//50
+remaining_of_50=remaining_of_100%50
+notes_of_10=remaining_of_50//10
+remaining_of_10=remaining_of_50%10
+print("The total amount of 100 taka notes: ",notes_of_100)
+print("The toal amount of 50 taka notes: ",notes_of_50)
+print("The total amount of 10 taka notes: ",notes_of_10)
+print("The rest money: ",remaining_of_10)
+'''
 
-print( "\n",message)
-do=achievement[0:-1]
-print(f"Very Good {name} \n You earned {do}")
+
+
