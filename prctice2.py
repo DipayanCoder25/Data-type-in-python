2866590d16871b6812e31989ba98184cf63e299c
DipayanCoder25 <dipayanprogramer3@gmail.com>
2025-03-26 15:39:38 +0600

    Add files via upload

--- prctice2.py
+++ prctice2.py
@@ -36,7 +36,7 @@ emi=(loan_amount*monthly_interast_rate*(1+monthly_interast_rate)**loan_tenure_mo
     (1+monthly_interast_rate**loan_tenure_monthly-1)
 print("Loan amount",loan_amount)
 print("Monthly amoun",round(emi,2))
-'''
+
 import math
 
 num = float(input("Enter a number to find its square root: "))
@@ -46,5 +46,74 @@ ans = math.sqrt(num)
 
 print(f"The square root of {num} is {ans}")
 
+loan_amount=input("The total loan amount: ")
+annual_interest_rate=input("Enter intarest rate: ")
+loan_tenure_years=input("Enter loan tenure year: ")
+monthly_interest_rate=annual_interest_rate/12/100
+loan_tenure_monthly=loan_tenure_years*12
+#EMI calculation formula
+emi=(loan_amount*monthly_interest_rate*(1+monthly_interest_rate)**loan_tenure_monthly)/ \
+    ((1+monthly_interest_rate)**loan_tenure_monthly-1)
+
+
+print("Loan Amount: ", loan_amount)
+print("Monthly EMI: ",round(emi,2))
+
+
+
+# Investment details
+principal = input("Enter invesment amount: ")  # Initial investment
+annual_rate = input("Enter interest perchentage:   ") # Interest rate in %
+times_compounded = 4  # Quarterly
+years = input("Enter invesment duration: ")  # Investment duration
+
+# Compound Interest Formula
+final_amount = principal * (1 + (annual_rate / (times_compounded * 100))) ** (times_compounded * years)
+
+print("Initial Investment:", principal)
+print("Final Amount after", years, "years:", round(final_amount, 2))
+print("Total Interest Earned:", round(final_amount - principal, 2))
+'''
+month_number = int(input("Enter running month number: "))
+temp = int(input("Enter today's temperature: "))
+
+
+if month_number >= 6 and month_number <= 8:
+    print("It's summer")
+    print("You should wear light clothes.")
+    if temp >= 30:
+        print("Temperature is hot today.")
+    elif temp <= 30:
+        print("Temperature is not bad.")
+
+
+elif month_number >= 3 and month_number <= 5:
+    print("It's spring")
+    print("You should wear light clothes.")
+    if temp >= 30:
+        print("Temperature is hot today.")
+    elif temp <= 30:
+        print("Temperature is not bad.")
+
+
+elif month_number >= 9 and month_number <= 11:
+    print("It's autumn")
+    print("Wear heavy clothes and jackets.")
+    if temp >= 30:
+        print("Temperature is hot today.")
+    elif temp <= 30:
+        print("Temperature is not bad.")
+
+
+elif month_number == 12 or month_number == 1 or month_number == 2:
+    print("It's winter")
+    print("Wear heavy clothes and jackets.")
+    if temp >= 30:
+        print("Temperature is hot today.")
+    elif temp <= 30:
+        print("Temperature is not bad.")
+
+else:
+    print("Invalid month number. Please enter a number between 1 and 12.")
 
 
